1906b24123e4bde096ac3031c5cdd0d64089f95e
maochd <maochengdong@watertek.com>
2021-11-09 16:10:17 +0800

    全局日志，自定义日志

---
+++ maochd-common/maochd-common-log/src/main/java/com/maochd/cloud/common/log/aop/LogCustomAspect.java
@@ -0,0 +1,54 @@
+package com.maochd.cloud.common.log.aop;
+
+import com.maochd.cloud.common.log.constant.LogConst;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
+@Aspect
+public class LogCustomAspect {
+
+    @Pointcut(value = "@annotation(com.maochd.cloud.common.log.annotation.CustomLog)")
+    public void servicePointCut() {
+    }
+
+    @SneakyThrows
+    @Around(value = "servicePointCut()")
+    public Object interceptor(ProceedingJoinPoint joinPoint) {
+        //从切面织入点处通过反射机制获取织入点处的方法
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        //获取切入点所在的方法
+        Method method = signature.getMethod();
+        // 获取参数名称
+        LocalVariableTableParameterNameDiscoverer paramNames = new LocalVariableTableParameterNameDiscoverer();
+        String[] params = paramNames.getParameterNames(method);
+        // 获取参数
+        List<Object> args = Arrays.asList(joinPoint.getArgs());
+        // 拼接参数
+        Map<String, Object> reqParams = IntStream.range(0, args.size())
+                .boxed()
+                .collect(Collectors.toMap(k -> Objects.requireNonNull(params)[k], args::get));
+        log.info(LogConst.LOG_START_MSG, method.getName(), null, reqParams);
+        // 获取开始时间
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        // 计算方法耗时
+        long diffTime = (System.currentTimeMillis() - startTime) / 1000;
+        log.info(LogConst.LOG_END_MSG, method.getName(), diffTime, result);
+        return result;
+    }
+}
