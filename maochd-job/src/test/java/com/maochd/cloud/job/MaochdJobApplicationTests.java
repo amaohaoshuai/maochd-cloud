a096ff2452582bc8f84df0583631930d86bd050f
maochd <maochengdong@watertek.com>
2021-11-10 15:32:15 +0800

    新增分布式定时任务

---
+++ maochd-job/src/test/java/com/maochd/cloud/job/MaochdJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maochd.cloud.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaochdJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
