eae25a18a11b24d33f030ba7062d5b925a1a4640
maochd <maochengdong@watertek.com>
2022-02-17 14:21:05 +0800

    集成mq

---
+++ maochd-mq/src/main/java/com/maochd/cloud/mq/MaochdMqApplication.java
@@ -0,0 +1,13 @@
+package com.maochd.cloud.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaochdMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaochdMqApplication.class, args);
+    }
+
+}
---
+++ maochd-mq/src/test/java/com/maochd/cloud/mq/MaochdMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maochd.cloud.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaochdMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
