339a1883ff1654c6f0304163cdcde1993d8a9212
maochd <maochd@aliyun.com>
2022-04-19 11:21:24 +0800

    补充必要注释

--- maochd-mq/src/main/java/com/maochd/cloud/mq/service/MessageProducer.java
+++ maochd-mq/src/main/java/com/maochd/cloud/mq/service/MessageProducer.java
@@ -2,5 +2,12 @@ package com.maochd.cloud.mq.service;
 
 public interface MessageProducer {
 
+    /**
+     * 向指定topic推送消息
+     *
+     * @param topic 主题
+     * @param obj   消息体
+     * @return boolean
+     */
     boolean pushMessage(String topic, Object obj);
 }
